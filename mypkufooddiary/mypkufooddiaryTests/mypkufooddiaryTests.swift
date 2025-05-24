aace44262d122b82d09d8f599408286fef379723
Mr. Homeless <3000goods@gmail.com>
2025-05-25 02:38:42 +0500

    xcode 프로젝트 파일 커밋

---
+++ mypkufooddiary/mypkufooddiaryTests/mypkufooddiaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  mypkufooddiaryTests.swift
+//  mypkufooddiaryTests
+//
+//  Created by Amor Mundis on 5/25/25.
+//
+
+import Testing
+@testable import mypkufooddiary
+
+struct mypkufooddiaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
